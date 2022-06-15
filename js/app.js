e89d011bc93b41a740f0c38495edf80dde832de0
nixbytes <real8686@gmail.com>
2022-06-14 21:52:16 -0400

    add viewpoint helper

--- js/app.js
+++ js/app.js
@@ -23,84 +23,109 @@
  *
  */
 
-const { nav, section } = getNavgation();
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-// Create array for each section
-
-list_sections = Array.from(document.getElementsByTagName("section"));
-
+ const {nav, section} = getNavgation();
+ 
+ /**
+  * End Global Variables
+  * Start Helper Functions
+  *
+  */
+ 
+ // Create array for each section
+ 
+ list_sections = Array.from(document.getElementsByTagName("section"));
+ 
 //  console.log(list_sections);
-
-// space sections with flex
-nav.style.cssText = "display: flex;text-align:center;justify-content: flex-end";
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav with each sections 1`
-list_sections.forEach((sections, index) => {
-  // create the li for the nav
-  let li = document.createElement("li");
-
-  // add attributes to each li element and add index value
-  li.setAttribute("id", "li-section" + (index + 1));
-  let anchor = document.createElement("a");
+ 
+ // space sections with flex
+ nav.style.cssText = "display: flex;text-align:center;justify-content: flex-end";
+ 
+ /**
+  * End Helper Functions
+  * Begin Main Functions
+  *
+  */
+ 
+ // build the nav with each sections 1`
+ list_sections.forEach((sections, index) => {
+   // create the li for the nav
+   let li = document.createElement("li");
+ 
+   // add attributes to each li element and add index value
+   li.setAttribute("id", "li-section" + (index + 1));
+   let anchor = document.createElement("a");
   //  console.log(`${sections} : ${index}`);
+ 
+   // sets all li elements with a class activeNav
+   li.setAttribute("class", "activeNav");
+ 
+   li.appendChild(anchor);
+   nav.appendChild(li);
+ 
+   // this jumps to each section on the page.
+   anchor.setAttribute("href", list_sections[index].innerHTML);
+ 
+   anchor.href = "#section" + (index + 1);
+ 
+   anchor.textContent = list_sections[index].dataset.nav;
+ 
+   // setting the anchor tag to have a class of 'menu__link' for the style
+   anchor.setAttribute("class", "menu__link");
+ });
+ 
+ // next we need to change the class for li on the navbar
+ // and set the first with your-active-class and others with activeNav
+ // the css/style.css for style
+ 
+ const updateLiClass = document.getElementById("li-section1");
+ updateLiClass.classList.add("your-active-class");
+ updateLiClass.classList.remove("activeNav");
+ 
+ const navLinks = document.querySelectorAll(".navbar__menu ul a");
 
-  // sets all li elements with a class activeNav
-  li.setAttribute("class", "activeNav");
+ // Scroll to anchor ID using scrollTO event
+ // Add class 'active' to section when near top of viewport
+ 
+ let onClick = function (event) {
+   // method cancels the event if it is cancelable
+   event.preventDefault();
+ 
+   const href = this.getAttribute("href");
+   // Get the offsetTop position of href
+   const getTop = document.querySelector(href).offsetTop;
+ 
+   // get last click navbar and remove active
+  //  const lastLi = document.querySelector(".activeNav");
+ 
+  //  // check for active element and remove the class
+ 
+  //  lastLi && lastLi.classList.remove("activeNav");
 
-  li.appendChild(anchor);
-  nav.appendChild(li);
+ 
+  //  const selectLi = document.querySelector(
+  //    "#li-section" + href.slice(href.indexOf("n") + 1)
+  //  );
+  //  selectLi.setAttribute("class", "activeNav");
+ 
+   window.scrollTo({
+     top: getTop,
+     behavior: "smooth",
+   });
+ };
+ 
+ navLinks.forEach((navl) => {
+   navl.addEventListener("click", onClick);
+ });
+ 
 
-  // this jumps to each section on the page.
-  anchor.setAttribute("href", list_sections[index].innerHTML);
+// helper function for check viewport
 
-  anchor.href = "#section" + (index + 1);
-
-  anchor.textContent = list_sections[index].dataset.nav;
-
-  // setting the anchor tag to have a class of 'menu__link' for the style
-  anchor.setAttribute("class", "menu__link");
-});
-
-const navLinks = document.querySelectorAll(".navbar__menu ul a");
-
-navLinks.forEach((navl) => {
-  navl.addEventListener("click", onClick);
-});
-
-// Scroll to anchor ID using scrollTO event
-// Add class 'active' to section when near top of viewport
-
-let onClick = function (event) {
-  const href = this.getAttribute("href");
-  // Get the offsetTop position of href
-  const getTop = document.querySelector(href).offsetTop;
-
-  // get last click navbar and remove active
-  const lastLi = document.querySelector(".activeNav");
-
-  // method cancels the event if it is cancelable
-  event.preventDefault();
-
-  window.scrollTo({
-    top: getTop,
-    behavior: "smooth",
-  });
-};
-
-function getNavgation() {
-  const nav = document.getElementById("navbar__list");
-  const section = document.querySelectorAll("section");
-  return { nav, section };
+ function isInViewport(section) {
+  const distance = section.getBoundingClientRect();
+  return (
+      distance.top >= 0 &&
+      distance.left >= 0 &&
+      distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
